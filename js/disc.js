a46ccf24909cc4d52dbb5beb6bbfe0e973bd9db1
Konrad Höffner <konrad@gmx.at>
2017-09-28 00:46:26 +0200

    alpha version of disc dash

--- js/disc.js
+++ js/disc.js
@@ -1,6 +1,7 @@
 const DISC_R_MIN = 10;
 const DISC_R_MAX = 100;
-const DISC_DURATION = 2000.0;
+const DISC_INNER_R_MAX = 20;
+const DISC_DURATION = 5000.0;
 const DISC_V = (2*DISC_R_MAX-DISC_R_MIN)/DISC_DURATION; // grow then shrink to zero size
 const DISC_T_MAX_R = (DISC_R_MAX-DISC_R_MIN)/DISC_V;
 
@@ -19,18 +20,27 @@ class Disc
 
   draw()
   {
-    context.fillStyle = "white";
+    if(!this.alive) {return;}
+    context.fillStyle = "yellow";
     context.beginPath();
     context.arc(this.x,this.y,this.r,0,2*Math.PI);
     context.fill();
+    /*
+    context.fillStyle = "red";
+    context.beginPath();
+    context.arc(this.x,this.y,Math.min(this.r,DISC_INNER_R_MAX),0,2*Math.PI);
+    context.fill();
+    */
   }
 
   update(dt)
   {
+    if(!this.alive) {return false;}
     this.t+=dt;
     if(this.t>DISC_DURATION)
     {
       this.alive=false;
+      this.clicked=false;
       return false;
     }
     if(this.t<DISC_T_MAX_R)
@@ -38,16 +48,22 @@ class Disc
       this.r = DISC_R_MIN + DISC_V*this.t;
     } else
     {
-      //this.r = DISC_R_MAX - DISC_V*(this.t-DISC_T_MAX_R);
+      this.r = DISC_R_MAX - DISC_V*(this.t-DISC_T_MAX_R);
     }
     return true;
   }
 
-  isHit(cursorX,cursorY)
+  click(cursorX,cursorY)
   {
-
+    console.log(cursorX);
+    console.log(cursorY);
+    if((Math.pow(cursorX-this.x,2)+Math.pow(cursorY-this.y,2))<this.r*this.r)
     {
+      this.alive = false;
+      this.clicked = true;
+      return true;
     }
+    return false;
   }
 }
 
